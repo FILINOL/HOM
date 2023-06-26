e53b0d4648630b707c093eb37573f2d7f42e2280
FILIN <hagi_beter@mail.ru>
2023-06-26 19:33:01 +0300

    for

--- nomber_10.py
+++ nomber_10.py
@@ -1,8 +1,17 @@
-coins = input().split()
-n = len(coins)
-heads = coins.count('H') 
-tails = n - heads 
-if heads <= tails:
-    print(heads) 
+coins = ["H", "T", "H", "H", "T", "T"]
+heads = 0
+tails = 0
+
+for coin in coins:
+    if coin == "H":
+        heads += 1
+    else:
+        tails += 1
+
+if heads == len(coins) or tails == len(coins):
+    flips = 0
 else:
-    print(tails) 
+    target = "H" if heads > tails else "T"
+    flips = sum(coin != target for coin in coins)
+
+print(flips)
